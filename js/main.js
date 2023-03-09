267eee3b3cfa699841d64d86a88f73ab6492e275
Hannah S <hanncstewa99@gmail.com>
2023-03-09 18:00:55 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// scroll to top button
+const topButton = document.getElementById("scroll-button");
+
+window.onscroll = function() {scrollFunction()}; 
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        topButton.style.display = "block"; 
+    } else {
+        topButton.style.display = "none"; 
+    }
+}
+
+//When user clicks button, scroll to top of page
+function topFunction() {
+    document.body.scrollTop = 0; //For Safari
+    document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE and Opera
+}
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  // contact form - clear text upon submission
+const btn = document.querySelector('.btn');
+const messageForm = document.getElementById('message');
+
+btn.addEventListener('click', setTimeout);
+
+setTimeout(resetForm, 1000);
+
+console.log(messageForm.value);
+
+function resetForm() {
+    messageForm.value = '';
+}
